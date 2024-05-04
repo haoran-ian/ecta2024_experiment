06e49975e255de93d4033bda04c507632cc766a1
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2024-05-04 15:48:00 +0200

    y transformations done

---
+++ transformations/y_scaling.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+
+if not os.path.exists("ecta2024_data/y_scaling/"):
+    os.makedirs("ecta2024_data/y_scaling/")
+
+for problem_id in range(1, 6):
+    file_path = f"ecta2024_data/origin/{problem_id}_0.000000.txt"
+    array = np.loadtxt(file_path)
+    for log_k in range(-30, 31, 1):
+        if log_k == 0:
+            continue
+        new_array = array * 2**(log_k/10)
+        np.savetxt(
+            f"ecta2024_data/y_scaling/{problem_id}_{log_k/10:.6f}.txt",
+            new_array)
---
+++ transformations/y_translation.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+
+if not os.path.exists("ecta2024_data/y_translation/"):
+    os.makedirs("ecta2024_data/y_translation/")
+
+for problem_id in range(1, 6):
+    file_path = f"ecta2024_data/origin/{problem_id}_0.000000.txt"
+    array = np.loadtxt(file_path)
+    for dy in range(50, 1001, 50):
+        new_array = array + dy
+        np.savetxt(
+            f"ecta2024_data/y_translation/{problem_id}_{dy}.txt", new_array)
