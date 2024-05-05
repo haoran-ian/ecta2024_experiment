4bbc9089cb1fcb944cb852212072b5be9e33c938
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2024-05-05 23:28:07 +0200

    add tvec table to db

--- scripts/tvec_database.py
+++ scripts/tvec_database.py
@@ -1,4 +1,7 @@
+import os
+import sqlite3
 import numpy as np
+import pandas as pd
 
 
 def generate_tvec(low, high):
@@ -12,14 +15,27 @@ def generate_tvec(low, high):
     return arr
 
 
-tvecs = []
-for i in range(50):
-    for j in range(20):
-        low = j * 5
-        high = low + 5
-        tvec = generate_tvec(low, high)
-        tvecs += [tvec.tolist()]
-np.savetxt("config/tvec.txt", np.array(tvecs))
-
-tvecs = np.loadtxt("config/tvec.txt")
-tvec_l1 = np.max(np.abs(tvecs), axis=1)
+if __name__ == "__main__":
+    if not os.path.exists("config/tvec.txt"):
+        print("Generating tvecs, file saved at config/tvec.txt")
+        tvecs = []
+        for i in range(50):
+            for j in range(20):
+                low = j * 5
+                high = low + 5
+                tvec = generate_tvec(low, high)
+                tvecs += [tvec.tolist()]
+        np.savetxt("config/tvec.txt", np.array(tvecs))
+    conn = sqlite3.connect("ecta2024_data/atom_data.db")
+    cursor = conn.cursor()
+    cursor.execute(f"select name from sqlite_master where type='table' and \
+        name='tvec'")
+    if cursor.fetchone():
+        cursor.execute(f"drop table tvec")
+    values = []
+    tvecs = np.loadtxt("config/tvec.txt")
+    tvec_l1 = np.max(np.abs(tvecs), axis=1)
+    for i in range(tvec_l1.shape[0]):
+        values += [[i, int(tvec_l1[i]/5)*5+5]]
+    df = pd.DataFrame(data=values, columns=["tvec_id", "translation distance"])
+    df.to_sql("tvec", conn, index=False)
